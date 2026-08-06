f2e35c07275c29350cc1ffe0c20724f4536dc72e
“zhouwenxing” <1804808430@qq.com>
2026-08-06 22:09:40 +0800

    更细

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,7 +2,3 @@
 BIAM Tests Module
 Unit tests for BIAM model components
 """
-
-__version__ = "1.0.0"
-__author__ = "Wenxing Zhou, Chao Xu, Jian Xiao, Jing Hu, Xuelin Zhang"
-__date__ = "September 7, 2025"
